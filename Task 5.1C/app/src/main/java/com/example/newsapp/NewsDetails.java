ff6b82bdf6f8627fddef7a748fbfc6841b5fd5f6
Prerna <shahiprerna94@gmail.com>
2024-04-09 21:46:45 +1000

    back button added

--- Task 5.1C/app/src/main/java/com/example/newsapp/NewsDetails.java
+++ Task 5.1C/app/src/main/java/com/example/newsapp/NewsDetails.java
@@ -1,7 +1,10 @@
 package com.example.newsapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -18,13 +21,23 @@ public class NewsDetails extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_news_details);
-
+        Button backtoMainBtn = findViewById(R.id.backBtn);
 
         String selectedItemDueDate = getIntent().getStringExtra("selectedDescription");
         //Set the values retrieved from MainActivity
         selectedDescriptionText = findViewById(R.id.selectedDescriptionText);
         Log.i("Test", selectedItemDueDate);
         selectedDescriptionText.setText(selectedItemDueDate);
+
+
+        backtoMainBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), MainActivity.class);
+                v.getContext().startActivity(intent);
+
+            }
+        });
 
     }
 }
