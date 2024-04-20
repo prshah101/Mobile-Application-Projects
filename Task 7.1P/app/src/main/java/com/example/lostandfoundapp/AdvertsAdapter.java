f3a6569bfe9885b2466c174b19f4bd7ea2d0167f
Prerna <shahiprerna94@gmail.com>
2024-04-20 22:37:52 +1000

    working on removing advert

--- Task 7.1P/app/src/main/java/com/example/lostandfoundapp/AdvertsAdapter.java
+++ Task 7.1P/app/src/main/java/com/example/lostandfoundapp/AdvertsAdapter.java
@@ -1,6 +1,7 @@
 package com.example.lostandfoundapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.lostandfoundapp.Advert;
 import com.example.lostandfoundapp.R;
+import com.google.android.material.card.MaterialCardView;
 
 import java.util.List;
 
@@ -35,6 +37,22 @@ public class AdvertsAdapter extends RecyclerView.Adapter<AdvertsAdapter.AdvertVi
     public void onBindViewHolder(@NonNull AdvertViewHolder holder, int position) {
         Advert advert = adverts.get(position);
         holder.bind(advert);
+
+
+        // Set click listener for the card
+        holder.card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), RemoveAdvertActivity.class);
+                // Retrieve the text from nameTextView
+                String name = holder.nameTextView.getText().toString();
+                intent.putExtra("Name", name);
+                // Retrieve the text from nameTextView
+                String lost = holder.isLostTextView.getText().toString();
+                intent.putExtra("Lost", lost);
+                v.getContext().startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -47,10 +65,13 @@ public class AdvertsAdapter extends RecyclerView.Adapter<AdvertsAdapter.AdvertVi
         private TextView nameTextView;
         private TextView isLostTextView;
 
+        private MaterialCardView card;
+
         public AdvertViewHolder(@NonNull View itemView) {
             super(itemView);
             nameTextView = itemView.findViewById(R.id.postTypeItemTv);
             isLostTextView = itemView.findViewById(R.id.postItemTv);
+            card = itemView.findViewById(R.id.card);
         }
 
         public void bind(Advert advert) {
--- Task 7.1P/app/src/main/java/com/example/lostandfoundapp/RemoveAdvertActivity.java
+++ Task 7.1P/app/src/main/java/com/example/lostandfoundapp/RemoveAdvertActivity.java
@@ -1,4 +1,59 @@
 package com.example.lostandfoundapp;
 
-public class RemoveAdvertActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RemoveAdvertActivity extends AppCompatActivity {
+
+    TextView postTypeItemTv2;
+    TextView postItemTv2;
+    TextView dateTv2;
+    TextView locationTv2;
+    Button removeBtn;
+
+    Button backBtn3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.remove_advert);
+
+        // Retrieve selected news ID and source information from MainActivity page
+        String name = getIntent().getStringExtra("Name");
+        String lost = getIntent().getStringExtra("Lost");
+
+        postTypeItemTv2 = findViewById(R.id.postTypeItemTv2);
+        postItemTv2 = findViewById(R.id.postItemTv2);
+        dateTv2 = findViewById(R.id.dateTv2);
+        locationTv2 = findViewById(R.id.locationTv2);
+        removeBtn = findViewById(R.id.removeBtn);
+        backBtn3 = findViewById(R.id.backBtn3);
+
+        postTypeItemTv2.setText(lost);
+        postTypeItemTv2.setText(name);
+
+        // Set OnClickListener for remove button
+        removeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(RemoveAdvertActivity.this, AllAdvertsActivity.class);
+//                startActivity(intent);
+            }
+        });
+
+        // Set OnClickListener for back button
+        backBtn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RemoveAdvertActivity.this, AllAdvertsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
