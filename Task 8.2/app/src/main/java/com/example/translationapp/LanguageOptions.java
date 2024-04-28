ab33d2bd6d50515f889aad725cfac2fcabda367c
Prerna <shahiprerna94@gmail.com>
2024-04-28 18:38:42 +1000

    spinners working

---
+++ Task 8.2/app/src/main/java/com/example/translationapp/LanguageOptions.java
@@ -0,0 +1,30 @@
+package com.example.translationapp;
+
+public class LanguageOptions {
+
+    static String[] fromlanguage = {
+            "From", "English", "Spanish", "French", "German", "Italian",
+            "Portuguese", "Dutch", "Russian", "Japanese", "Chinese",
+            "Korean", "Arabic", "Hindi", "Bengali", "Urdu",
+            "Punjabi", "Turkish", "Vietnamese", "Thai", "Indonesian",
+            "Greek", "Swedish", "Norwegian", "Finnish", "Danish",
+            "Polish", "Czech", "Hungarian", "Romanian", "Ukrainian",
+            "Bulgarian", "Serbian", "Croatian", "Slovak", "Slovenian",
+            "Macedonian", "Albanian", "Estonian", "Latvian", "Lithuanian",
+            "Maltese", "Icelandic", "Farsi", "Hebrew", "Swahili",
+            "Filipino", "Malay", "Kurdish", "Gujarati"
+    };
+
+    static String[] tolanguage = {
+            "To", "English", "Spanish", "French", "German", "Italian",
+            "Portuguese", "Dutch", "Russian", "Japanese", "Chinese",
+            "Korean", "Arabic", "Hindi", "Bengali", "Urdu",
+            "Punjabi", "Turkish", "Vietnamese", "Thai", "Indonesian",
+            "Greek", "Swedish", "Norwegian", "Finnish", "Danish",
+            "Polish", "Czech", "Hungarian", "Romanian", "Ukrainian",
+            "Bulgarian", "Serbian", "Croatian", "Slovak", "Slovenian",
+            "Macedonian", "Albanian", "Estonian", "Latvian", "Lithuanian",
+            "Maltese", "Icelandic", "Farsi", "Hebrew", "Swahili",
+            "Filipino", "Malay", "Kurdish", "Gujarati"
+    };
+}
--- Task 8.2/app/src/main/java/com/example/translationapp/MainActivity.java
+++ Task 8.2/app/src/main/java/com/example/translationapp/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.translationapp;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,43 +29,147 @@ public class MainActivity extends AppCompatActivity {
     private TextView translateLanguageTV;
     private Button translateLanguageBtn;
 
+    //String[] fromlanguage = LanguageOptions.fromlanguage;
+    //String[] tolanguage = LanguageOptions.tolanguage;
+
+    String[] fromLanguages = {"From", "English", "Afrikaans", "Arabic", "Belarusian", "Bulgarian", "Bengali", "Catalan", "Czech", "Welsh", "Hindi", "Urdu"};
+
+    String[] toLanguages = {"To", "English", "Afrikaans", "Arabic", "Belarusian", "Bulgarian", "Bengali", "Catalan", "Czech", "Welsh", "Hindi", "Urdu"};
+
     // create a variable for our
     // firebase language translator.
     FirebaseTranslator englishGermanTranslator;
+
+    private static final int REQUEST_PERMISSION_CODE = 1;
+    int languageCode, fromLanguageCode, toLanguageCode = 8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // on below line we are creating our firebase translate option.
-        FirebaseTranslatorOptions options =
-                new FirebaseTranslatorOptions.Builder()
-                        // below line we are specifying our source language.
-                        .setSourceLanguage(FirebaseTranslateLanguage.EN)
-                        // in below line we are displaying our target language.
-                        .setTargetLanguage(FirebaseTranslateLanguage.DE)
-                        // after that we are building our options.
-                        .build();
-        // below line is to get instance
-        // for firebase natural language.
-        englishGermanTranslator = FirebaseNaturalLanguage.getInstance().getTranslator(options);
+        Spinner fromSpinner = findViewById(R.id.idFromSpinner);
+        Spinner toSpinner = findViewById(R.id.idToSpinner);
 
-        // on below line we are initializing our variables.
-        edtLanguage = findViewById(R.id.idEdtLanguage);
-        translateLanguageTV = findViewById(R.id.idTVTranslatedLanguage);
-        translateLanguageBtn = findViewById(R.id.idBtnTranslateLanguage);
-
-        // adding on click listener for button
-        translateLanguageBtn.setOnClickListener(new View.OnClickListener() {
+        fromSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                // calling method to download language
-                // modal to which we have to translate.
-                String string = edtLanguage.getText().toString();
-                downloadModal(string);
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                fromLanguageCode = getLanguageCode(fromLanguages[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
             }
         });
+
+        ArrayAdapter<String> fromAdapter = new ArrayAdapter<>(MainActivity.this, R.layout.spinner_item, fromLanguages);
+
+        fromAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        fromSpinner.setAdapter(fromAdapter);
+
+        toSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                toLanguageCode = getLanguageCode(toLanguages[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        ArrayAdapter<String> toAdapter = new ArrayAdapter<>(MainActivity.this, R.layout.spinner_item, toLanguages);
+
+        toAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        toSpinner.setAdapter(toAdapter);
+        //
+
+//        // on below line we are creating our firebase translate option.
+//        FirebaseTranslatorOptions options =
+//                new FirebaseTranslatorOptions.Builder()
+//                        // below line we are specifying our source language.
+//                        .setSourceLanguage(FirebaseTranslateLanguage.EN)
+//                        // in below line we are displaying our target language.
+//                        .setTargetLanguage(FirebaseTranslateLanguage.DE)
+//                        // after that we are building our options.
+//                        .build();
+//        // below line is to get instance
+//        // for firebase natural language.
+//        englishGermanTranslator = FirebaseNaturalLanguage.getInstance().getTranslator(options);
+//
+//        // on below line we are initializing our variables.
+//        edtLanguage = findViewById(R.id.idEdtLanguage);
+//        translateLanguageTV = findViewById(R.id.idTVTranslatedLanguage);
+//        translateLanguageBtn = findViewById(R.id.idBtnTranslateLanguage);
+//
+//        // adding on click listener for button
+//        translateLanguageBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                //translatedTV.setText("");
+//                if (edtLanguage.getText().toString().isEmpty()) {
+//                    Toast.makeText(MainActivity.this, "Please enter your text to translate", Toast.LENGTH_SHORT).show();
+//                } else if(fromLanguageCode == 0) {
+//                    Toast.makeText(MainActivity.this, "Please select language of the input text", Toast.LENGTH_SHORT).show();
+//                } else if(toLanguageCode == 0) {
+//                    Toast.makeText(MainActivity.this, "Please select the language to translation into", Toast.LENGTH_SHORT).show();
+//                }
+//                else{
+//                    // calling method to download language
+//                    // modal to which we have to translate.
+//                    String string = edtLanguage.getText().toString();
+//                    downloadModal(string);
+//                }
+//            }
+//        });
+    }
+
+    public int getLanguageCode(String language) {
+
+        int languageCode = 8;
+
+        switch (language) {
+
+            case "English":
+                languageCode = FirebaseTranslateLanguage.EN;
+                break;
+            case "Afrikaans":
+                languageCode = FirebaseTranslateLanguage.AF;
+                break;
+            case "Arabic":
+                languageCode = FirebaseTranslateLanguage.AR;
+                break;
+            case "Belarusian":
+                languageCode = FirebaseTranslateLanguage.BE;
+                break;
+            case "Bengali":
+                languageCode = FirebaseTranslateLanguage.BN;
+                break;
+            case "Catalan":
+                languageCode = FirebaseTranslateLanguage.CA;
+                break;
+
+            case "Czech":
+                languageCode = FirebaseTranslateLanguage.CS;
+                break;
+
+            case "Welsh":
+                languageCode = FirebaseTranslateLanguage.CY;
+                break;
+
+            case "Hindi":
+                languageCode = FirebaseTranslateLanguage.HI;
+                break;
+
+            case "Urdu":
+                languageCode = FirebaseTranslateLanguage.UR;
+                break;
+
+            default:
+        }
+        return languageCode;
     }
 
     private void downloadModal(String input) {
