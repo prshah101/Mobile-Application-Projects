fe586054a3a46e83e2c8e80186229fb7414beea4
Prerna <shahiprerna94@gmail.com>
2024-05-13 17:42:35 +1000

    upgraded retrofit

--- Task 8.1C/app/src/main/java/com/example/chatbot/Chat.java
+++ Task 8.1C/app/src/main/java/com/example/chatbot/Chat.java
@@ -8,10 +8,14 @@ import android.widget.EditText;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+//import com.android.volley.Request;
+//import com.android.volley.RequestQueue;
+//import com.android.volley.toolbox.StringRequest;
+//import com.android.volley.toolbox.Volley;
 
 public class Chat extends AppCompatActivity {
     private Button loginBtn;
@@ -23,15 +27,15 @@ public class Chat extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_chat);
 
-        //fetchChatDetails();
+        fetchChatDetails();
     }
 
-//    private void fetchTriviaQuestions() {
-//        Retrofit retrofit = new Retrofit.Builder()
-//                .baseUrl("http://10.0.2.2:5000/")
-//                .addConverterFactory(GsonConverterFactory.create())
-//                .client(new OkHttpClient.Builder().readTimeout(10, java.util.concurrent.TimeUnit.MINUTES).build()) // this will set the read timeout for 10mins (IMPORTANT: If not your request will exceed the default read timeout)
-//                .build();
-//
-//    }
+    private void fetchChatDetails() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://10.0.2.2:5000/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(new OkHttpClient.Builder().readTimeout(10, java.util.concurrent.TimeUnit.MINUTES).build()) // this will set the read timeout for 10mins (IMPORTANT: If not your request will exceed the default read timeout)
+                .build();
+        
+    }
 }
