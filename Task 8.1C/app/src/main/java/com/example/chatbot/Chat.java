90e9944d53ed8e6844158cc488ea0d20fea9f114
Prerna <shahiprerna94@gmail.com>
2024-05-13 17:34:25 +1000

    Authentication working

--- Task 8.1C/app/src/main/java/com/example/chatbot/Chat.java
+++ Task 8.1C/app/src/main/java/com/example/chatbot/Chat.java
@@ -1,4 +1,20 @@
 package com.example.chatbot;
 
-public class Chat {
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Chat extends AppCompatActivity {
+    private Button loginBtn;
+    private EditText usernameEnter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_chat);
+    }
 }
