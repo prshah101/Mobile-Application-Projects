c83d766d5162e7df958e3a3ab454529caf3fc6da
Prerna <shahiprerna94@gmail.com>
2024-05-20 18:47:20 +1000

    Multiple locations enabled

--- Task 9.1P/app/src/main/java/com/example/lostandfoundv2/ShowMapActivity.java
+++ Task 9.1P/app/src/main/java/com/example/lostandfoundv2/ShowMapActivity.java
@@ -10,6 +10,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class ShowMapActivity extends AppCompatActivity implements OnMapReadyCallback {
@@ -26,8 +27,28 @@ public class ShowMapActivity extends AppCompatActivity implements OnMapReadyCall
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
-        LatLng location = new LatLng(55.6761, 12.5683);
-        googleMap.addMarker(new MarkerOptions().position(location).title("Copenhagen"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 12));
+        LatLng location1 = new LatLng(55.6761, 12.5683); // Copenhagen
+        LatLng location2 = new LatLng(48.8566, 2.3522);  // Paris
+        LatLng location3 = new LatLng(51.5074, -0.1278); // London
+
+        googleMap.addMarker(new MarkerOptions().position(location1).title("Copenhagen"));
+        googleMap.addMarker(new MarkerOptions().position(location2).title("Paris"));
+        googleMap.addMarker(new MarkerOptions().position(location3).title("London"));
+
+        // Create a LatLngBounds builder to include all the markers
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+        builder.include(location1);
+        builder.include(location2);
+        builder.include(location3);
+
+        // Get the LatLngBounds
+        LatLngBounds bounds = builder.build();
+
+        // Adjust the padding as needed (in pixels)
+        int padding = 100; // Offset from edges of the map in pixels
+
+        // Move and animate the camera to show all markers within the bounds
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, padding));
     }
+
 }
